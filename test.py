62e4d4805a06658719681d466ff7675dd3d6ff9b
Kateryna Yefremova <118860905+ktrfrm@users.noreply.github.com>
2024-11-10 22:22:50 -0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+# Your first and last name
+print("K   K"+"   "+"Y   Y")
+print("K  K "+"   "+" Y Y ")
+print("K K  "+"   "+"  Y  ")
+print("KK   "+"   "+"  Y  ")
+print("K K  "+"   "+"  Y  ")
+print("K  K "+"   "+"  Y  ")
+print("K   K"+"   "+"  Y  ")
+
+# A fun fact about yourself
